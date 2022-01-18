6ae4dec5cf6d6d2ac0840b305dfc0b6dd6089456
hydrophyl <forever_mind@live.com>
2022-01-18 17:41:39 +0100

    apply light theme on gmap

--- pages/index.tsx
+++ pages/index.tsx
@@ -14,6 +14,90 @@ const Home = () => {
       map = new google.maps.Map(document.getElementById("map") as HTMLElement, {
         center: { lat: 50.88, lng: 10.23 },
         zoom: 7,
+        fullscreenControl: false, // remove the top-right button
+        mapTypeControl: false, // remove the top-left buttons
+        streetViewControl: false, // remove the pegman
+        zoomControl: false, // remove the bottom-right buttons
+        styles: [
+          {
+            featureType: "administrative",
+            elementType: "labels.text.fill",
+            stylers: [
+              {
+                color: "#444444",
+              },
+            ],
+          },
+          {
+            featureType: "landscape",
+            elementType: "all",
+            stylers: [
+              {
+                color: "#f2f2f2",
+              },
+            ],
+          },
+          {
+            featureType: "poi",
+            elementType: "all",
+            stylers: [
+              {
+                visibility: "off",
+              },
+            ],
+          },
+          {
+            featureType: "road",
+            elementType: "all",
+            stylers: [
+              {
+                saturation: -100,
+              },
+              {
+                lightness: 45,
+              },
+            ],
+          },
+          {
+            featureType: "road.highway",
+            elementType: "all",
+            stylers: [
+              {
+                visibility: "simplified",
+              },
+            ],
+          },
+          {
+            featureType: "road.arterial",
+            elementType: "labels.icon",
+            stylers: [
+              {
+                visibility: "off",
+              },
+            ],
+          },
+          {
+            featureType: "transit",
+            elementType: "all",
+            stylers: [
+              {
+                visibility: "off",
+              },
+            ],
+          },
+          {
+            featureType: "water",
+            elementType: "all",
+            stylers: [
+              {
+                color: "#67e8f9",
+              },
+              {
+                visibility: "on",
+              },
+            ],
+          },
+        ],
       });
     });
   });
