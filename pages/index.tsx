38981c2d07cacb4035b888947724d0216e01cc80
hydrophyl <forever_mind@live.com>
2022-01-19 14:13:59 +0100

    success add svg icon for marker on map

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,10 @@
 import React, { useEffect, useRef } from "react";
 import { Loader } from "@googlemaps/js-api-loader";
 
+var template = [
+  '<?xml version="1.0"?>',
+  '<svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 svg-shadow-yellow" viewBox="0 0 20 20" fill="#facc15"> <path fillRule="evenodd" d="M5.05 4.05a7 7 0 119.9 9.9L10 18.9l-4.95-4.95a7 7 0 010-9.9zM10 11a2 2 0 100-4 2 2 0 000 4z" clipRule="evenodd" /> </svg>',
+].join("\n");
 const Home = () => {
   const MAP_API_KEY = process.env.NEXT_PUBLIC_API_KEY as string;
   useEffect(() => {
@@ -12,8 +16,8 @@ const Home = () => {
     loader.load().then(() => {
       const google = window.google;
       map = new google.maps.Map(document.getElementById("map") as HTMLElement, {
-        center: { lat: 50.88, lng: 10.23 },
-        zoom: 7,
+        center: { lat: 51.2, lng: 10.23 },
+        zoom: 8,
         fullscreenControl: false, // remove the top-right button
         mapTypeControl: false, // remove the top-left buttons
         streetViewControl: false, // remove the pegman
@@ -90,7 +94,7 @@ const Home = () => {
             elementType: "all",
             stylers: [
               {
-                color: "#67e8f9",
+                color: "#e0f2fe",
               },
               {
                 visibility: "on",
@@ -99,6 +103,25 @@ const Home = () => {
           },
         ],
       });
+      let markerOptions = {
+        position: new google.maps.LatLng(53.597981, 9.957736),
+        optimized: false,
+      };
+      var docMarker = new google.maps.Marker({
+        position: new google.maps.LatLng(53.597981, 9.957736),
+        map: map,
+        icon: {
+          url:
+            "data:image/svg+xml;charset=UTF-8," + encodeURIComponent(template),
+          scaledSize: new google.maps.Size(20, 20),
+        },
+        optimized: false,
+      });
+      var overlay = new google.maps.OverlayView();
+      overlay.draw = function () {
+        this.getPanes().markerLayer.id = "svg-glow";
+      };
+      overlay.setMap(map);
     });
   });
   return (
