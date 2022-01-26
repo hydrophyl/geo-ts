22b742fa20809c13c17367233e200cf5c2086bc2
hydrophyl <forever_mind@live.com>
2022-01-26 18:57:35 +0100

    migrate styling to external files

---
+++ constants/styles.ts
@@ -0,0 +1,87 @@
+export var svg = [
+  '<?xml version="1.0"?>',
+  '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="{{ color }}"> <path fillRule="evenodd" d="M5.05 4.05a7 7 0 119.9 9.9L10 18.9l-4.95-4.95a7 7 0 010-9.9zM10 11a2 2 0 100-4 2 2 0 000 4z" clipRule="evenodd" /> </svg>',
+].join("\n");
+export var svg_yellow = svg.replace("{{ color }}", "#facc15");
+export var svg_red = svg.replace("{{ color }}", "#f87171");
+export var svg_lime = svg.replace("{{ color }}", "#a3e635");
+export const mapStyleLight = [
+  {
+    featureType: "administrative",
+    elementType: "labels.text.fill",
+    stylers: [
+      {
+        color: "#444444",
+      },
+    ],
+  },
+  {
+    featureType: "landscape",
+    elementType: "all",
+    stylers: [
+      {
+        color: "#f2f2f2",
+      },
+    ],
+  },
+  {
+    featureType: "poi",
+    elementType: "all",
+    stylers: [
+      {
+        visibility: "off",
+      },
+    ],
+  },
+  {
+    featureType: "road",
+    elementType: "all",
+    stylers: [
+      {
+        saturation: -100,
+      },
+      {
+        lightness: 45,
+      },
+    ],
+  },
+  {
+    featureType: "road.highway",
+    elementType: "all",
+    stylers: [
+      {
+        visibility: "simplified",
+      },
+    ],
+  },
+  {
+    featureType: "road.arterial",
+    elementType: "labels.icon",
+    stylers: [
+      {
+        visibility: "off",
+      },
+    ],
+  },
+  {
+    featureType: "transit",
+    elementType: "all",
+    stylers: [
+      {
+        visibility: "off",
+      },
+    ],
+  },
+  {
+    featureType: "water",
+    elementType: "all",
+    stylers: [
+      {
+        color: "#e0f2fe",
+      },
+      {
+        visibility: "on",
+      },
+    ],
+  },
+];
---
+++ pages/api/get-aes.ts
@@ -0,0 +1,15 @@
+import { NextApiHandler } from 'next';
+import { query } from '../../lib/db';
+
+const handler: NextApiHandler = async (_, res) => {
+  try {
+    const results = await query(`
+      SELECT lat, lng, ae_countcontract FROM tbl_postcodes, tbl_mapapi WHERE ae_postcode=postcode
+  `);
+    return res.json(results);
+  } catch (e) {
+    res.status(500).json({ message: e.message });
+  }
+};
+
+export default handler;
