fb9d5662931e786166e55324cd159be8c5eb032e
hydrophyl <forever_mind@live.com>
2022-03-09 09:16:18 +0100

    fix db connection and add blue points

--- lib/db.ts
+++ lib/db.ts
@@ -6,7 +6,10 @@ export const db = mysql({
     database: process.env.MYSQL_DATABASE,
     user: process.env.MYSQL_USERNAME,
     password: process.env.MYSQL_PASSWORD,
-    port: parseInt(process.env.MYSQL_PORT)
+    port: parseInt(process.env.MYSQL_PORT),
+    ssl  : {
+      rejectUnauthorized: false
+    }
   }
 });
 
