71cb26ae591432e8bbadc3650b3f135a2f537a55
hydrophyl <forever_mind@live.com>
2022-03-28 23:53:53 +0200

    change table source from databank

--- pages/api/get-aes.ts
+++ pages/api/get-aes.ts
@@ -4,7 +4,7 @@ import { query } from '../../lib/db';
 const handler: NextApiHandler = async (_, res) => {
   try {
     const results = await query(`
-      SELECT lat, lon, ae_timediff FROM tbl_postcodes, tbl_mapapi WHERE ae_postcode=postcode
+      SELECT lat, lon, ae_timediff FROM tbl_postcodes, tbl_fahrradbatterien WHERE ae_postcode=postcode
     `);
     return res.json(results);
   } catch (e) {
