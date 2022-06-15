b0a8944726bce41b237ed1c9a2179abcf9cb458a
hydrophyl <forever_mind@live.com>
2022-06-15 10:33:23 +0200

    clear mockup file

--- update_backend.sh
+++ update_backend.sh
@@ -1,12 +1,10 @@
 #!/bin/bash
-
 #run the scripts to update backend of the geo-ts frontend google map for "überfällige Aufträge"
 update ()
 {
-				python fetch_grs_online.py
-				python update_postcodes.py
+	python fetch_grs_online.py
+	python update_postcodes.py
 }
-
 while true ; do
 update
 sleep 1h
